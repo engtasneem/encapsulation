4792f089d17db4cf8b3af486715bf8a61000cb57
engtasneem <134233991+engtasneem@users.noreply.github.com>
2023-05-22 20:21:26 +0300

    Add files via upload

---
+++ ConsoleApplication1/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Person
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Emaill { get; set; }
+        public string FullName;
+       public void  GetFullName()
+        {
+            FullName = Firstname + " " + Lastname;
+            
+        }
+        public void print()
+        {
+            Console.WriteLine("The FullName is: "+ FullName);
+            Console.WriteLine("The Emaill is :"+Emaill);
+            
+        }
+
+
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.Firstname = "Noor";
+            p.Lastname = "Ali";
+            p.Emaill = "should contain @ and end with .com";
+            p.GetFullName();
+            p.print();
+
+
+
+
+        }
+    }
+}
